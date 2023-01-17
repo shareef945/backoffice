9091c28962d7fe468e6726b7031495b303481146
Shareef Ali <shareef945@gmail.com>
2023-01-17 17:11:25 +0000

    wip

--- src/components/pages/landingPage.jsx
+++ src/components/pages/landingPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function LandingPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div
       style={{
